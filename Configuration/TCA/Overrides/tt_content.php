31f3ffcbe0e978e9a661a58502e5b5975f48700f
Achim Fritz <achim.fritz@b13.de>
2019-12-14 15:36:05 +0100

    [TASK] init

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,19 @@
+<?php
+
+\B13\Container\TcaRegistry::registerContainer(
+    'b13-2cols-with-header-container',
+    '2 Column Container With Header',
+    'Some Description of the Container',
+    'container_example',
+    [
+        [
+            ['name' => 'header', 'colPos' => 200, 'colspan' => 2]
+        ],
+        [
+            ['name' => 'left side', 'colPos' => 201],
+            ['name' => 'right side', 'colPos' => 202]
+        ]
+    ]
+);
+
+$GLOBALS['TCA']['tt_content']['types']['b13-2cols-with-header-container']['showitem'] = 'sys_language_uid,CType,header,tx_container_parent,colPos';
---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Container Example',
+    'description' => 'Example for EXT:container',
+    'category' => 'misc',
+    'author' => 'b13 GmbH',
+    'author_email' => 'typo3@b13.de',
+    'author_company' => 'b13 GmbH',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'clearCacheOnLoad' => true,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [],
+        'conflicts' => [],
+        'suggests' => []
+    ]
+];
+
