4e72c3d1041176ab2742c6a61011a716857d8bd4
SvenJuergens <SvenJuergens@users.noreply.github.com>
2020-06-16 22:39:54 +0200

    [TASK] use addStaticFile with sys_template to add TypoScript Configuration

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,7 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'container_example',
+    'Configuration/TypoScript',
+    'Container Example TypoScript Configuration'
+);
