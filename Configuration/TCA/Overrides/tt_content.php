db82b09a4ba135895ca18c01b5d0041d528a8058
Achim Fritz <achim.fritz@b13.de>
2021-11-12 23:20:23 +0100

    [TASK] a LLL example

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -4,11 +4,11 @@
     (
         new \B13\Container\Tca\ContainerConfiguration(
             'b13-2cols-with-header-container', // CType
-            '2 Column Container With Header', // label
-            'Some Description of the Container', // description
+            'LLL:EXT:container/Resources/Private/Language/locallang.xlf:container', // label
+            'LLL:EXT:container/Resources/Private/Language/locallang.xlf:container', // description
             [
                 [
-                    ['name' => 'header', 'colPos' => 200, 'colspan' => 2, 'allowed' => ['CType' => 'header, textmedia']]
+                    ['name' => 'LLL:EXT:container/Resources/Private/Language/locallang.xlf:container', 'colPos' => 200, 'colspan' => 2, 'allowed' => ['CType' => 'header, textmedia']]
                 ],
                 [
                     ['name' => 'left side', 'colPos' => 201],
