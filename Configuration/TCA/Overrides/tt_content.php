df61fc81f06d397b7a02b77c94f389d285373de3
Matthias Vogel <m.vogel@andersundsehr.com>
2026-03-20 15:04:16 +0100

    [TASK] add ContentArea example

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -56,3 +56,19 @@ $GLOBALS['TCA']['tt_content']['types']['b13-2cols-with-header-container']['showi
         )
     )->setRegisterInNewContentElementWizard(false)
 );
+
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)->configureContainer(
+    (
+    new \B13\Container\Tca\ContainerConfiguration(
+        'b13-2cols-content-area', // CType
+        '2 Column (ContentArea)', // label
+        'Some Description of the Container', // description
+        [
+            [
+                ['name' => '2-cols-left', 'colPos' => 200],
+                ['name' => '2-cols-right', 'colPos' => 201]
+            ]
+        ] // grid configuration
+    )
+    )->setBackendTemplate('EXT:container_example/Resources/Private/Templates/BackendTemplate.html')
+);
